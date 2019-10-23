09c8fc42cf507e9bffe014ac87c97414c878aa47
Elena Shishkina <ellensshi@gmail.com>
2019-10-23 16:43:08 +0300

    login from any page

--- test_product_page.py
+++ test_product_page.py
@@ -38,3 +38,19 @@ def test_guest_can_add_product_to_basket(browser, promo_code):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
+
+
+@pytest.mark.step8
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.step8
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
