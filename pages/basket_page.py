570ecd17fdb7c54ac55f29c5cf9be9433313e335
Elena Shishkina <ellensshi@gmail.com>
2019-10-24 16:38:22 +0300

    correct locator for empty basket

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -7,5 +7,5 @@ class BasketPage(BasePage):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Basket contains items, but should not"
 
     def should_be_empty_basket_message(self):
-        assert "empty" in self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_MESSAGE).text, \
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), \
             "Empty basket message should be presented, but did not"
